2b914606a7606cbbe8e388484dbc0f2122f188d6
alevi1 <alevin@sapient.com>
2016-07-18 17:17:42 -0400

    moved style to www/css file to simplify. should add classes or unique identifiers later

--- src/app/Board.js
+++ src/app/Board.js
@@ -1,32 +1,9 @@
 import React from "react";
 
-const styles = {
-  tr: {
-    margin: 0,
-    padding: 0
-  },
-  alive: {
-    border: "1px solid #ddd",
-    width: 18,
-    height: 18,
-    margin: 0,
-    padding: 0,
-    background: "blue"
-  },
-  dead: {
-    border: "1px solid #ddd",
-    width: 18,
-    height: 18,
-    margin: 0,
-    padding: 0,
-    background: "white"
-  }
-};
-
 const Cell = (props, alive) => (
   <td
   key={props}
-  style={alive.length ? styles.alive : styles.dead}
+  data-status={alive.length ? "alive" : "dead"}
   ></td>
 );
 
@@ -35,7 +12,7 @@ const Row = ({width=20, height=20, cells=[], row=0}) => {
   for(let w = 0; w < width; w++) {
     cols.push(Cell(w+"-"+row, cells[row][w]));
   }
-  return <tr key={"row"+row} style={styles.tr}>{cols}</tr>;
+  return <tr key={"row"+row} >{cols}</tr>;
 };
 
 const Board = ({width=20, height=20, cells=[]}) => {
