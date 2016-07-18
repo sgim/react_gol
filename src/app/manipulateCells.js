d0c0a19e6a5812cf3bda99195992b646f0158a11
alevi1 <alevin@sapient.com>
2016-07-18 16:59:31 -0400

    better caching of variables in manipulateCells function

--- src/app/manipulateCells.js
+++ src/app/manipulateCells.js
@@ -1,26 +1,29 @@
-export default (cells) => cells.map((row, i) => {
-  // get count of surrounding neighbors for each cell
-  return row.map((cell, j) => {
-    cell.neighbors = 0;
-    let startRow = i === 0 ? i : i - 1;
-    let endRow = i + 1 === cells.length ? i: i + 1;
-    let startCol = j === 0 ? j : j - 1;
-    let endCol = j + 1 === row.length ? j : j + 1;
-    for(; startRow <= endRow; startRow++) {
-      for(let col = startCol; col <= endCol; col++) {
-        let neighbor = cells[startRow][col];
-        neighbor.length && (neighbor !== cell) && cell.neighbors++;
+export default (cells) => {
+  let height = cells.length;
+  let width = cells[0].length;
+  return cells.map((row, i) => {
+    // get count of surrounding neighbors for each cell
+    let endRow = i + 1 === height ? i: i + 1;
+    let startR = i === 0 ? i : i - 1;
+    return row.map((cell, j) => {
+      cell.neighbors = 0;
+      let startCol = j === 0 ? j : j - 1;
+      let endCol = j + 1 === width ? j : j + 1;
+      for(let startRow = startR; startRow <= endRow; startRow++) {
+        for(let col = startCol; col <= endCol; col++) {
+          let neighbor = cells[startRow][col];
+          neighbor.length && (neighbor !== cell) && cell.neighbors++;
+        }
       }
+      return cell;
+    });
+    // determine whether to keep cell alive or dead depending on neighbor count
+  }).map(row => row.map(cell => {
+    let n = cell.neighbors;
+    let alive = cell.length;
+    if(!alive) {
+      return n === 3 ? [0]: [];
     }
-    return cell;
-  });
-  // determine whether to keep cell alive or dead depending on neighbor count
-}).map(row => row.map(cell => {
-  let n = cell.neighbors;
-  let alive = cell.length;
-  if(!alive) {
-    return n === 3 ? [0]: [];
-  }
-  return (n === 3 || n === 2) ? [0]: [];
-}));
-
+    return (n === 3 || n === 2) ? [0]: [];
+  }));
+};
